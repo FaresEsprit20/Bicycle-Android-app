4eabcbb567201b17bea4d36022b696f923445aaf
Fares <fares.benslama@esprit.tn>
2025-03-30 05:24:36 +0100

    first commit

--- app/src/main/java/com/example/bicycle/dao/BikeDao.java
+++ app/src/main/java/com/example/bicycle/dao/BikeDao.java
@@ -1,27 +1,26 @@
 package com.example.bicycle.dao;
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 import com.example.bicycle.models.Bike;
 
 import java.util.List;
 
-
 @Dao
 public interface BikeDao {
 
-    @Query("SELECT * FROM bike_table")
+    @Query("SELECT * FROM bike_table") // Add Query annotation
     List<Bike> getAll();
 
     @Insert
     void insertOne(Bike bike);
 
-    @Query("DELETE FROM bike_table")
-    public void deleteAll();
+    @Query("DELETE FROM bike_table") // Add Query annotation
+    void deleteAll();
 
     @Delete
     void delete(Bike bike);
-
-
 }
